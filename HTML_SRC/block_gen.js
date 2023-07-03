44b819282306dfe0bc85fa7c9a025b0dc5c9486f
VlasovAlexey <a.vlasov@gaijin.team>
2023-07-03 19:34:37 +0300

    changes

--- HTML_SRC/block_gen.js
+++ HTML_SRC/block_gen.js
@@ -1,45 +1,42 @@
 function Slider(selector, currentId) {
-			var self = this;
-			this.duration = 200;
-			this.obj = $(selector);
+	var self = this;
+	this.duration = 200;
+	this.obj = $(selector);
 
-			this.ctrl =
-					this.obj.find('.header').each(function(i){
-						$(this).attr({'data-slider-id': i});
-					});
-
-			this.body =
-					this.obj.find('.content').each(function(i){
-						$(this).attr({'data-slider-id': i});
-					})
-					.css({display: 'none'});
-
-			this.current = -1;
-
-			this.ctrl.click(function(e) {
-				e.preventDefault();
-				self.open($(this).attr('data-slider-id'), e );
-
+	this.ctrl =
+			this.obj.find('.header').each(function(i){
+				$(this).attr({'data-slider-id': i});
 			});
 
-		}
+	this.body =
+			this.obj.find('.content').each(function(i){
+				$(this).attr({'data-slider-id': i});
+			})
+			.css({display: 'none'});
 
-		/*Slider.prototype.open = function(i,e){
-			if (this.current === i) return false;
-			if (this.current>-1) {
-				//this.body.eq(this.current).slideUp(this.duration);
-			}
+	this.current = -1;
 
-			this.current = i;
+	this.ctrl.click(function(e) {
+		e.preventDefault();
+		self.open($(this).attr('data-slider-id'), e );
 
-			//this.body.eq(this.current).slideDown(this.duration,function(){(e.currentTarget.scrollIntoView(true)) + 30});
-            this.body.eq(this.current).slideDown(this.duration);
+	});
 
-		};
-		*/
-Slider.prototype.open = function (i, e) {
-	this.body.eq(i).slideToggle(this.duration);
+}
+
+Slider.prototype.open = function(i,e){
+	if (this.current === i) return false;
+	if (this.current>-1) {
+		this.body.eq(this.current).slideUp(this.duration);
+	}
+
+	this.current = i;
+	//,function(){e.currentTarget.scrollIntoView(true)} added for move header to top of the page
+		this.body.eq(this.current).slideDown(this.duration,function(){(e.currentTarget.scrollIntoView(true)) + 30});
+	//this.body.eq(this.current).slideDown(this.duration);
+
 };
+
 
 $(function() {
 	new Slider('#slider', 0);
