3bfaffdb57d3c9fec36d29ccbb13428e6a77c5ca
VlasovAlexey <a.vlasov@gaijin.team>
2023-08-23 21:46:56 +0300

    Fixed Errors on Android

--- compass-app-main/code.js
+++ compass-app-main/code.js
@@ -52,6 +52,7 @@
 
   //IOS compass sensor reading
   window.addEventListener('deviceorientation', function(e) {
+    if(e.webkitCompassHeading != NaN){
       if(popupShown){
         popupShown = false;
         document.getElementById("accessblur").style.opacity = "0";
@@ -118,86 +119,92 @@
         directionName = "NW"
       } 
       document.getElementById("heading-name").innerHTML = directionName;
+    }
+      
   }, false);
 
   //Android compass sensor reading
-  let status  = document.getElementById('status');
+  let Vstatus  = document.getElementById('status');
   if ( 'AbsoluteOrientationSensor' in window ) {
     //compass.hidden = false;     
     let sensor = new AbsoluteOrientationSensor();
     sensor.addEventListener('reading', function(el) {
       let q = el.target.quaternion;
-      Aheading = Math.atan2(2*q[0]*q[1] + 2*q[2]*q[3], 1 - 2*q[1]*q[1] - 2*q[2]*q[2])*(180/Math.PI);
-      if(popupShown){
-        popupShown = false;
-        document.getElementById("accessblur").style.opacity = "0";
-        updatePosition();
-      }
-    
-      var levelDisp = document.getElementById("level-disp");
-      var levelX = 0; var levelY = 0;
-      var levelG = Math.min(Math.max((el.gamma / 9), -5), 5);
-      var levelB = Math.min(Math.max((el.beta / 9), -5), 5);
-    
-      var heading = 0;
-
-      var screenAngle = window.orientation;
-      if(screenAngle == 0 || screenAngle == 360) { // rightside up
-        heading = (360 - Aheading);
-        levelY = levelB;
-        levelX = levelG;
-      } else if(screenAngle == 90) { //landscape left
-        heading = (270 - Aheading);
-        levelY = levelG * -1;
-        levelX = levelB;
-      } else if(screenAngle == 180) { //upside down
-        heading = (180 - Aheading);
-        levelY = levelB * -1;
-        levelX = levelG * -1;
-      } else if(screenAngle == 270 || screenAngle == -90) { //landscape right
-        heading = (90 - Aheading);
-        levelY = levelG;
-        levelX = levelB * -1;
-      } else {
-        console.error("The browser don`t support window.orientation");
-      }
-      levelDisp.style.top = (levelY + 50) + "%";
-      levelDisp.style.left = (levelX + 50) + "%";
-      var AlabelAngle = 360-heading;
+      if(q != NaN){
+        Aheading = Math.atan2(2*q[0]*q[1] + 2*q[2]*q[3], 1 - 2*q[1]*q[1] - 2*q[2]*q[2])*(180/Math.PI);
+        if(popupShown){
+          popupShown = false;
+          document.getElementById("accessblur").style.opacity = "0";
+          updatePosition();
+        }
       
-      const labels = document.querySelectorAll(".label");
-      for (let i = 0; i < labels.length; i++) {
-        labels[i].style.transform = "translate(-50%, -50%) rotate(" + AlabelAngle + "deg";
+        var levelDisp = document.getElementById("level-disp");
+        var levelX = 0; var levelY = 0;
+        var levelG = Math.min(Math.max((el.gamma / 9), -5), 5);
+        var levelB = Math.min(Math.max((el.beta / 9), -5), 5);
+      
+        var heading = 0;
+  
+        var screenAngle = window.orientation;
+        if(screenAngle == 0 || screenAngle == 360) { // rightside up
+          heading = (360 - Aheading);
+          levelY = levelB;
+          levelX = levelG;
+        } else if(screenAngle == 90) { //landscape left
+          heading = (270 - Aheading);
+          levelY = levelG * -1;
+          levelX = levelB;
+        } else if(screenAngle == 180) { //upside down
+          heading = (180 - Aheading);
+          levelY = levelB * -1;
+          levelX = levelG * -1;
+        } else if(screenAngle == 270 || screenAngle == -90) { //landscape right
+          heading = (90 - Aheading);
+          levelY = levelG;
+          levelX = levelB * -1;
+        } else {
+          console.error("The browser don`t support window.orientation");
+        }
+        levelDisp.style.top = (levelY + 50) + "%";
+        levelDisp.style.left = (levelX + 50) + "%";
+        var AlabelAngle = 360-heading;
+        
+        const labels = document.querySelectorAll(".label");
+        for (let i = 0; i < labels.length; i++) {
+          labels[i].style.transform = "translate(-50%, -50%) rotate(" + AlabelAngle + "deg";
+        }
+        dial.style.transform = "rotate(" + heading + "deg)"
+        var AacHeading = 360 - Math.round(heading);
+        if(AacHeading >= 360){
+          AacHeading -= 360;
+        }
+        document.getElementById("heading-value").innerHTML = AacHeading + "&deg";
+        var AdirectionName = "";
+        if(AacHeading > 337 || AacHeading < 22){
+          AdirectionName = "N"
+        } else if(AacHeading < 67){
+          AdirectionName = "NE"
+        } else if(AacHeading < 112){
+          AdirectionName = "E"
+        } else if(AacHeading < 157){
+          AdirectionName = "SE"
+        } else if(AacHeading < 202){
+          AdirectionName = "S"
+        } else if(AacHeading < 247){
+          AdirectionName = "SW"
+        } else if(AacHeading < 292){
+          AdirectionName = "W"
+        } else {
+          AdirectionName = "NW"
+        } 
+        document.getElementById("heading-name").innerHTML = AdirectionName;
       }
-      dial.style.transform = "rotate(" + heading + "deg)"
-      var AacHeading = 360 - Math.round(heading);
-      if(AacHeading >= 360){
-        AacHeading -= 360;
-      }
-      document.getElementById("heading-value").innerHTML = AacHeading + "&deg";
-      var AdirectionName = "";
-      if(AacHeading > 337 || AacHeading < 22){
-        AdirectionName = "N"
-      } else if(AacHeading < 67){
-        AdirectionName = "NE"
-      } else if(AacHeading < 112){
-        AdirectionName = "E"
-      } else if(AacHeading < 157){
-        AdirectionName = "SE"
-      } else if(AacHeading < 202){
-        AdirectionName = "S"
-      } else if(AacHeading < 247){
-        AdirectionName = "SW"
-      } else if(AacHeading < 292){
-        AdirectionName = "W"
-      } else {
-        AdirectionName = "NW"
-      } 
-      document.getElementById("heading-name").innerHTML = AdirectionName;
     });
     sensor.start();
   }
-  else status.innerHTML = 'AbsoluteOrientationSensor not supported';
+  else{
+    Vstatus.innerHTML = 'AbsoluteOrientationSensor not supported';
+  } 
 
   function convertCoordinates(latitude, longitude) {
   const latDegrees = Math.floor(latitude);
