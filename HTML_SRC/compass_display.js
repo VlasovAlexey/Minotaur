d751dd4ac40d4a041319c2c77563772d2d7cd972
VlasovAlexey <a.vlasov@gaijin.team>
2024-04-15 21:05:09 +0300

    add3

--- HTML_SRC/compass_display.js
+++ HTML_SRC/compass_display.js
@@ -201,8 +201,8 @@ function compass_upd(compass_data){
 			acHeading = 360 + acHeading
 		}
 
-		cross_01.style.transform = "translate(-50%, -50%) rotate(" + (heading + rot_dif) + "deg)"
-		cross_02.style.transform = "translate(-50%, -50%) rotate(" + (heading + rot_dif) + "deg)"
+		cross_01.style.transform = "translate(-50%, -50%) rotate(-" + (heading + rot_dif) + "deg)"
+		cross_02.style.transform = "translate(-50%, -50%) rotate(-" + (heading + rot_dif) + "deg)"
 	} else {
 		//ios
 		labelAngle = (Math.abs(360 - heading));
