4634d9d4991b94cae1227024481f791cc082768f
VlasovAlexey <all3862000@mail.ru>
2024-10-15 21:38:03 +0300

    test03

--- HTML_SRC/track_recorder.js
+++ HTML_SRC/track_recorder.js
@@ -159,7 +159,7 @@ function btn_record() {
 		z = [];
 		c = [];
 		for (i = 0; i < route_map_disp.length; i++) {
-			x.push((route_map_disp[i][0]));
+			x.push((route_map_disp[i][0]*(-1.0)));
 			y.push((route_map_disp[i][1]));
 			z.push((route_map_disp[i][2]));
 			c.push(i);
