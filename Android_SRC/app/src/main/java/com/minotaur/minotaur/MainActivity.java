9bf92e1834ddaccc0f11b90db6507ceaaca2e63f
VlasovAlexey <a.vlasov@gaijin.team>
2025-08-19 13:19:25 +0300

    Added file opening feature from Android device

--- Android_SRC/app/src/main/java/com/minotaur/minotaur/MainActivity.java
+++ Android_SRC/app/src/main/java/com/minotaur/minotaur/MainActivity.java
@@ -1,22 +1,29 @@
 package com.minotaur.minotaur;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.WindowManager;
 import android.webkit.GeolocationPermissions;
 import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -29,25 +36,46 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     WebView myWeb;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private ValueCallback<Uri[]> filePathCallback;
+    private static final int FILE_CHOOSER_REQUEST_CODE = 1;
+    String myURL = "file:///android_asset/index.html";
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Fixed portrait orientation
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        myWeb = findViewById(R.id.myWeb);
 
-        String myURL = "file:///android_asset/index.html";
+        //setup WebViewChromeClient for working with geolocation, file chooser and blob downloader
+        setupWebView();
+
+        //and finally start index.html from storage
+        myWeb.loadUrl(myURL);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    private void setupWebView() {
+        WebSettings settings = myWeb.getSettings();
+
+        // Enable JavaScript and file access
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setAllowFileAccessFromFileURLs(true);
 
         android.webkit.CookieManager cookieManager = android.webkit.CookieManager.getInstance();
         cookieManager.setAcceptCookie(true);
         cookieManager.acceptCookie();
         cookieManager.getCookie(myURL);
-
-        myWeb = findViewById(R.id.myWeb);
-
-        //enable access to local storage from web view
-        WebSettings settings = myWeb.getSettings();
-        settings.setJavaScriptEnabled(true);
         settings.setDatabaseEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setAllowContentAccess(true);
@@ -70,7 +98,13 @@ public class MainActivity extends AppCompatActivity {
         myWeb.setHorizontalScrollBarEnabled(false);
         myWeb.setVerticalScrollBarEnabled(false);
 
+        //enable geolocation services
+        myWeb.getSettings().setGeolocationEnabled(true);
+
+        // Set WebView clients
+        myWeb.setWebViewClient(new WebViewClient());
         myWeb.setWebChromeClient(new WebChromeClient() {
+            //override geolocation prompts
             @Override
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
                 if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -79,15 +113,17 @@ public class MainActivity extends AppCompatActivity {
                     LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                     Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     if (location != null) {
-                        //String latitude = String.valueOf(location.getLatitude());
-                        //String longitude = String.valueOf(location.getLongitude());
-                        //String url = myURL + "?lat=" + latitude + "&long=" + longitude;
+                        /* String latitude = String.valueOf(location.getLatitude());
+                        String longitude = String.valueOf(location.getLongitude());
+                        String url = myURL + "?lat=" + latitude + "&long=" + longitude;
+                         */
                         myWeb.loadUrl(myURL);
                         callback.invoke(origin, true, true);
                     } else {
                         callback.invoke(origin, false, false);
                     }
                 }
+                callback.invoke(origin, true, false);
             }
             @Override
             public void onPermissionRequest(PermissionRequest request) {
@@ -97,36 +133,58 @@ public class MainActivity extends AppCompatActivity {
                     super.onPermissionRequest(request);
                 }
             }
+            //display in console javascript errors and warnings
             @Override
             public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
                 android.util.Log.d("WebView--> ", consoleMessage.message());
                 return true;
             }
-        });
 
+            //parameters for file chooser
+            public boolean onShowFileChooser(WebView webView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+                if (MainActivity.this.filePathCallback != null) {
+                    MainActivity.this.filePathCallback.onReceiveValue(null);
+                }
+                MainActivity.this.filePathCallback = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    startActivityForResult(intent, FILE_CHOOSER_REQUEST_CODE);
+                } catch (Exception e) {
+                    MainActivity.this.filePathCallback = null;
+                    //Toast.makeText(MainActivity.this, "Cannot open file chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+        });
         // Check if location permission is granted
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // Request location permission if not granted
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
         }
 
-        //enable geolocation services
-        myWeb.getSettings().setGeolocationEnabled(true);
-        myWeb.setWebChromeClient(new WebChromeClient() {
-            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
-                callback.invoke(origin, true, false);
+
+    }
+    //file chooser override parameters
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != FILE_CHOOSER_REQUEST_CODE || filePathCallback == null) {
+            return;
+        }
+
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            String dataString = data.getDataString();
+            if (dataString != null) {
+                results = new Uri[]{Uri.parse(dataString)};
             }
-        });
+        }
 
-
-        //and finally start index.html from storage
-        myWeb.loadUrl(myURL);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
     }
 }
