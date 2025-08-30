99311fa1fefe5a3db4b39e66c9cd8f71151824d2
VlasovAlexey <a.vlasov@gaijin.team>
2025-08-30 20:10:29 +0300

    Changes for Google Play approve

--- Android_SRC/app/build.gradle.kts
+++ Android_SRC/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
     defaultConfig {
         applicationId = "com.minotaur.minotaur"
         minSdk = 31
-        targetSdk = 34
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
